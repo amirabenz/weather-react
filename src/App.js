962a76ee90503f1b139583f5b379bd8f51e87782
amirabenz <amira.benzid09@gmail.com>
2022-03-11 14:36:07 +0100

    from sandbox to react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        <div className="col-12">
+          <form id="search-form">
+            <input type="text" placeholder="search" id="searched-city" />
+            <button type="submit" id="search-button">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </button>
+            <button
+              type="button"
+              className="btn btn-outline-warning"
+              id="here-now"
+            >
+              current location
+            </button>
+          </form>
+        </div>
+        <div className="col-12 actual-city" id="here">
+          Krakow, PL
+        </div>
+        <div className="actual-date">Last updated: Mon 21:53</div>
+      </div>
+      <div className="actual-icon">
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+          alt=""
+          className="img-icon"
+        />
+      </div>
+      <div className="weather-description">Clear</div>
+      <div className="row today">
+        <div className="col-6 actual-temperature">
+          <div className="row degree">
+            <div className="col-2" />
+            <div className="col-6 t">
+              <span id="temp">3</span>
+            </div>
+            <div className="col-4 c">
+              <a href="#" id="celsius">
+                | <strong>°C </strong>
+              </a>
+              <br />
+
+              <a href="#" id="fahrenheit">
+                °F
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-6 other-infos">
+          <ul>
+            <li>
+              Humidity
+              <div>
+                <span className="humidity">70</span>%
+              </div>
+            </li>
+            <li>
+              Wind
+              <div>
+                <span className="wind"></span>
+                <span className="wind-unit"> 6 Km/h</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
